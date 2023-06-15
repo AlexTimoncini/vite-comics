8c77368240d7384353c4709de15da0dd1e7b3e51
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-15 16:01:11 +0200

    renamed main app into footer app

---
+++ src/components/FooterApp.vue
@@ -0,0 +1,18 @@
+<script>
+    import FooterNavList from './FooterNavList.vue';
+    export default{
+        name: 'FooterApp',
+        components: {
+            MainNavList,
+        }
+    }
+</script>
+
+<template>
+    <footer>
+        <FooterNavList />
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+</style>
