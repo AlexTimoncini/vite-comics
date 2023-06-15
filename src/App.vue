45568451afb9ea55786209a29685df207a0bad4a
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-15 14:49:28 +0200

    added header component (void) and imported it in App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script>
+<script>  
+  import HeaderApp from './components/HeaderApp.vue';
+  export default{
+    name: 'App',
+    components: {
+      HeaderApp,
+    }
+  }
 </script>
 
 <template>
+  <HeaderApp />
 </template>
 
 <style lang="scss">
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,16 @@
+<script>
+    export default{
+        name: 'HeaderApp'
+    }
+</script>
+
+<template>
+    <header>
+        
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/mixins' as *;
+    @use '../styles/partials/variables' as *;
+</style>
