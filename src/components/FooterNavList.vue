3229ff7d99f0e6da42cc88d16ab2e728560e8b24
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-15 16:28:20 +0200

    added style scss to footer navbar + big logo

--- src/components/FooterNavList.vue
+++ src/components/FooterNavList.vue
@@ -137,9 +137,12 @@
                 <ul class="ivy_navbar_list" v-for="list in navbarLists">
                     <h1 class="ivy_navbar_title">{{ list.title }}</h1>
                     <li class="ivy_navbar_item" v-for="item in list.navbarList">
-                        <a href="{{ item.link }}">item.name</a>
+                        <a href="{{ item.link }}">{{ item.name }}</a>
                     </li>
                 </ul>
+            </div>
+            <div class="ivy_big_logo">
+                <img src="../assets/img/dc-logo-bg.png" alt="dc logo">
             </div>
         </div>
     </nav>
@@ -150,6 +153,35 @@
     @use '../styles/partials/variables' as *;
     nav{
         background-image: url('../assets/img/footer-bg.jpg');
-        
+        overflow: hidden;
+        height: 450px;
+        .ivy_container{
+            @include flex(row, space-between, center, nowrap);
+            height: 100%;
+            .ivy_navbar_lists{
+                @include flex(column, flex-start, flex-start, wrap);
+                height: 100%;
+                padding-top: 2rem;
+                column-gap: 2rem;
+                z-index: 2;
+                .ivy_navbar_list{
+                    margin-bottom: 2rem;
+                    .ivy_navbar_title{
+                        color: $priFontColor;
+                        text-transform: uppercase;
+                        font-size: 1.3rem;
+                        margin-bottom: 1rem;
+                    }
+                    .ivy_navbar_item{
+                        padding: 0.25rem 0;
+
+                        a{
+                            color: $secFontColor
+                        }
+                    }
+                }
+            }
+        }
+
     }
 </style>
