b9befff71301adce37af265efd32221c4ad66d7f
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-16 13:03:25 +0200

    modifed jumbotron componenet

--- src/components/HeaderJumbo.vue
+++ src/components/HeaderJumbo.vue
@@ -6,20 +6,13 @@
 
 <template>
     <section id="jumbotron">
-        <div class="ivy_container">
-            <h1> -- Content goes here --</h1>
-        </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
-    @use '../styles/partials/variables' as *;
     #jumbotron{
-        background-color: $priBgColor;
-        color: $priFontColor;
-        h1{
-            padding: 4rem 0;
-            font-size: 2rem;
-        }
+        height: 500px;
+        background-image: url(../assets/img/jumbotron.jpg);
+        background-size: cover;
     }
 </style>
