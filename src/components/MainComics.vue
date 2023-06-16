04d4511fec2d7a07eb800db2517677f4410070d6
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-16 13:13:23 +0200

    added comics secton and imported it in 'mainApp.vue'

---
+++ src/components/MainComics.vue
@@ -0,0 +1,37 @@
+<script>
+    export default{
+        name: 'MainComics'
+    }
+</script>
+
+<template>
+    <section id="comics">
+        <div class="ivy_container">
+            <h1 class="ivy_comics_title">current series</h1>
+            <ul class="ivy_comic_list">
+    
+            </ul>
+            <button class="ivy_load_more">load more</button>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/mixins' as *;
+    @use '../styles/partials/variables' as *;
+
+    #comics{
+        position: relative;
+        padding: 2rem 0;
+        background-color: $priBgColor;
+        .ivy_comics_title{
+            text-transform: uppercase;
+            background-color: $logoColor;
+            color: $priFontColor;
+            padding: 1rem;
+            position: absolute;
+            top: 0;
+            transform: translateY(-50%);
+        }
+    }
+</style>
--- src/components/mainApp.vue
+++ src/components/mainApp.vue
@@ -1,15 +1,18 @@
 <script>
-import MainNavbar from './MainNavbar.vue';
+    import MainNavbar from './MainNavbar.vue';
+    import MainComics from './MainComics.vue';
     export default{
         name: 'MainApp',
         components: {
             MainNavbar,
+            MainComics
         }
     }
 </script>
 
 <template>
     <main>
+        <MainComics />
         <MainNavbar />
     </main>
 </template>
