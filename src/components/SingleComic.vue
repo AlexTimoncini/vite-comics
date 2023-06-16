08c04b6a897669005e1b5bf2ed73685e7ee9b2bb
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-16 13:40:35 +0200

    addded sngli comic component and imported with a vue for on a array of objects in main comics

---
+++ src/components/SingleComic.vue
@@ -0,0 +1,37 @@
+<script>
+    export default{
+        name: 'SingleComic',
+        props: {
+            comicObj : Object,
+        }
+    }
+</script>
+
+<template>
+    <li>
+        <div class="ivy_thumb">
+            <img :src="comicObj.thumb" :alt="comicObj.series">
+        </div>
+        <h2 class="ivy_comic_title">{{ comicObj.series }}</h2>
+    </li>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    .ivy_thumb{
+        height: 300px;
+        width: 100%;
+        margin-bottom: 1rem;
+
+        img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            display: block;
+        }
+    }
+
+    .ivy_comic_title{
+        color: $priFontColor;
+    }
+</style>
